9a21b718e99de9fa3aeba11c070e4b8a04ed5006
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-10-12 12:02:39 +0300

    Add files via upload

---
+++ Data Types and Variables/Data Types and Variables - Lab/concatenateNames.js
@@ -0,0 +1,5 @@
+function concatenateNames(firstName, secondName, delimeter) {
+    let result = `${firstName}${delimeter}${secondName}`
+    console.log(result);
+}
+concatenateNames('John', 'Smith', '->');
