89e70068f0fd765e958316c6228b55020f99ae63
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-10-13 08:38:16 +0300

    Add files via upload

---
+++ Data Types and Variables/Data Types and Variables - Lab/echoType.js
@@ -0,0 +1,13 @@
+function echoType(parameter) {
+    if (typeof parameter === "number") {
+        console.log("number");
+        console.log(parameter);
+    } else if (typeof parameter === "string") {
+        console.log("string");
+        console.log(parameter);
+    } else {
+        console.log("object");
+        console.log("Parameter is not suitable for printing");
+    }
+}
+echoType(null);
