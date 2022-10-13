e812e16ad21fe62c2a208e95915b6c12199a8a21
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-10-13 08:39:00 +0300

    Add files via upload

---
+++ Data Types and Variables/Data Types and Variables - Lab/integerAndFloat.js
@@ -0,0 +1,8 @@
+function integerAndFloat(firstNumber, secondNumber, thirdNumber) {
+    let sum = firstNumber + secondNumber + thirdNumber;
+    sum % 1 === 0
+        ? sum += ` - Integer`
+        : sum += ` - Float`;
+        console.log(sum);
+}
+integerAndFloat(100, 200, 303);
