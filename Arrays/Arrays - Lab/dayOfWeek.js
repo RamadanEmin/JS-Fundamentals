ee35736f64adc16b1a0865b3c2645f045479df4b
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-10-18 00:04:40 +0300

    Add files via upload

---
+++ Arrays/Arrays - Lab/dayOfWeek.js
@@ -0,0 +1,17 @@
+function dayOfWeek(number) {
+    let days = [
+        `Monday`,
+        `Tuesday`,
+        `Wednesday`,
+        `Thursday`,
+        `Friday`,
+        `Saturday`,
+        `Sunday `
+    ];
+    if (1 <= number && number <= 7) {
+        console.log(days[number - 1]);
+    } else {
+        console.log("Invalid day!");
+    }
+}
+dayOfWeek(7);
