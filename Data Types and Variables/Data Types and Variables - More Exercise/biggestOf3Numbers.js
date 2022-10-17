2c80f9db1c2d2299245f0bbd57f8ef8c7528b30c
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-10-17 21:43:30 +0300

    Create biggestOf3Numbers.js

---
+++ Data Types and Variables/Data Types and Variables - More Exercise/biggestOf3Numbers.js
@@ -0,0 +1,12 @@
+function biggestOf3Numbers(num1, num2, num3) {
+    let maxNumber = Number.MIN_SAFE_INTEGER;
+    if (num1 > maxNumber) {
+        maxNumber = num1;
+    } if (num2 > maxNumber) {
+        maxNumber = num2;
+    } if (num3 > maxNumber) {
+        maxNumber = num3;
+    }
+    console.log(maxNumber);
+}
+biggestOf3Numbers(-2, 7, 3);
