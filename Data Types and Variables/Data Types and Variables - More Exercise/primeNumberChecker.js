7bbd937b74a3e6c5f568ab5605a961e6878a2c84
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-10-18 00:02:21 +0300

    Add files via upload

---
+++ Data Types and Variables/Data Types and Variables - More Exercise/primeNumberChecker.js
@@ -0,0 +1,13 @@
+function primeNumberChecker(number) {
+    let result = true;
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            result = false;
+            break;
+        } 
+    }
+    console.log(result);
+}
+primeNumberChecker(7);
+primeNumberChecker(8);
+primeNumberChecker(81);
