8541a8fc3c2a10c84962e99aa9c88968a6ce6e6d
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-10-18 00:02:42 +0300

    Add files via upload

---
+++ Data Types and Variables/Data Types and Variables - More Exercise/triangleArea.js
@@ -0,0 +1,6 @@
+function triangleArea(sideA, sideB, sideC){
+let semiperimeter = (sideA+sideB+sideC)/2;
+let area = Math.sqrt(semiperimeter*(semiperimeter-sideA)*(semiperimeter-sideB)*(semiperimeter-sideC));
+console.log(area);
+}
+triangleArea(2,3.5,4);
