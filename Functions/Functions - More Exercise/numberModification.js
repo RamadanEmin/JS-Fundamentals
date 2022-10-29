3e4715d4650fc77f15014cc0f0be81a680961045
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-10-29 10:20:03 +0300

    Add files via upload

---
+++ Functions/Functions - More Exercise/numberModification.js
@@ -0,0 +1,26 @@
+function numberModification(modifiedNumber) {
+    function getSum(number) {
+        let numToString = number.toString();
+        let sum = 0;
+        for (let i = 0; i < numToString.length; i++) {
+            sum += Number(numToString[i]);
+        }
+        return sum;
+    }
+
+    function getAverage(num) {
+        return getSum(num) / num.toString().length;
+    }
+
+    while (getAverage(modifiedNumber) < 5) {
+        modifiedNumber += '9';
+    }
+    console.log(modifiedNumber);
+    //    function numberModification(number) {
+    //        const getDigitsAverage = num => num.toString().split('')
+    //            .map(Number).reduce((a, b) => a + b, 0) / num.toString().length;
+    //    while (getDigitsAverage(number) < 5) number += '9';
+    //    console.log(number);
+    //}
+}
+numberModification(101);
