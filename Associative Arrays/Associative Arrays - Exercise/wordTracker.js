016d9970d88d21c43b61f919a7a1f12092e0e0c1
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-11-30 09:34:37 +0200

    Add files via upload

---
+++ Associative Arrays/Associative Arrays - Exercise/wordTracker.js
@@ -0,0 +1,18 @@
+function wordTracker(input = []) {
+    let words = {};
+    let targetWords = input.shift().split(` `);
+    targetWords.forEach(word => words[word] = 0);
+    input.forEach(word => {
+        if (targetWords.includes(word)) {
+            words[word] += 1;
+        }
+    })
+    Object.entries(words)
+        .sort((a, b) => b[1] - a[1])
+        .forEach(entry => console.log(`${entry[0]} - ${entry[1]}`));
+}
+wordTracker([
+    'this sentence',
+    'In', 'this', 'sentence', 'you', 'have', 'to', 'count', 'the', 'occurrences', 'of',
+    'the', 'words', 'this', 'and', 'sentence', 'because', 'this', 'is', 'your', 'task'
+]);
